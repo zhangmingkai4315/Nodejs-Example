1b2c7e7107ea78b7ff77c2c818bc923ec4f2b8aa
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2015-09-11 18:38:22 +0800

    Proxy.js
    Proxy.js

---
+++ os/1.js
@@ -0,0 +1,15 @@
+var os = require('os');
+console.log(os.tmpdir());   //返回默认的temp目录
+console.log(os.hostname().toString('utf8'));
+console.log(os.endianness());   //windows LE
+console.log(os.type());  //windows_NT
+console.log(os.platform());  //win32
+console.log(os.arch());  //X64
+
+console.log(os.release());
+console.log(os.loadavg()); //windos 返回【0,0,0】
+
+console.log(os.freemem());
+console.log(os.uptime()/3600);
+console.log(os.networkInterfaces());
+console.log(os.cpus());
